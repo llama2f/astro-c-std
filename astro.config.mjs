8ea81788b652e6d57884266e0412c3e028fa97c6
caori sato <caori.st.1120@gmail.com>
2024-08-05 10:58:20 +0900

    breadcrumbs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
-import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://c-std.com',
-  integrations: [preact()]
+	site: 'https://c-std.com',
+	integrations: [preact(), sitemap()],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@import "src/styles/_mixin.scss";`,
+				},
+			},
+		},
+	},
 });
