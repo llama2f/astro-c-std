c8cfa336b6bce2d09545d9d7e8a6a3a00f04c780
caori sato <caori.st.1120@gmail.com>
2024-09-10 14:57:53 +0900

    seoのdescriptionとか

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,30 @@
-import mdx from '@astrojs/mdx'
-import preact from '@astrojs/preact'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import wikiLinkPlugin from '@portaljs/remark-wiki-link'
-import { defineConfig } from 'astro/config'
-import remarkLinkCard from 'remark-link-card'
+import mdx from '@astrojs/mdx';
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import wikiLinkPlugin from '@portaljs/remark-wiki-link';
+import { defineConfig } from 'astro/config';
+import remarkLinkCard from 'remark-link-card';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://c-std.com',
-  integrations: [preact(), sitemap(), mdx(), tailwind()],
+  integrations: [preact(), sitemap(), mdx(), tailwind(), partytown()],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "src/styles/_mixin.scss";`,
-        },
-      },
-    },
+          additionalData: `@import "src/styles/_mixin.scss";`
+        }
+      }
+    }
   },
   markdown: {
-    remarkPlugins: [
-      wikiLinkPlugin,
-      [
-        remarkLinkCard,
-        {
-          cache: true,
-          shortenUrl: true,
-        },
-      ],
-    ],
-  },
-})
+    remarkPlugins: [wikiLinkPlugin, [remarkLinkCard, {
+      cache: true,
+      shortenUrl: true
+    }]]
+  }
+});
