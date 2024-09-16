e962e5ee3b82e77df486ff8d487cbd43f2041159
caori sato <caori.st.1120@gmail.com>
2024-09-16 19:19:02 +0900

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+import mdx from '@astrojs/mdx'
+import preact from '@astrojs/preact'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import wikiLinkPlugin from '@portaljs/remark-wiki-link'
+import { defineConfig } from 'astro/config'
+import remarkLinkCard from 'remark-link-card'
+
+import partytown from '@astrojs/partytown'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://c-std.com',
+  integrations: [preact(), sitemap(), mdx(), tailwind(), partytown()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/_mixin.scss";`,
+        },
+      },
+    },
+  },
+  markdown: {
+    remarkPlugins: [
+      wikiLinkPlugin,
+      [
+        remarkLinkCard,
+        {
+          cache: true,
+          shortenUrl: true,
+        },
+      ],
+    ],
+  },
+})
